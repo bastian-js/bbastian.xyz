729c1279454ce806815bdf2d4e06190d28aaab9a
bastian <104389376+bastian-js@users.noreply.github.com>
2022-10-24 20:41:55 +0200

    update

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -14,10 +14,8 @@
     $to = "contact@knuddel.xyz";
 
     $headers = "From: $email_from \r\n";
-
+    
     $headers .= "Reply-To: $visitor_email \r\n";
 
     mail($to, $email_subject, $email_body, $headers);
-
-    header("Location: contact.html");
 ?>
