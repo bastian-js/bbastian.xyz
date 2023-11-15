65dbf7b62fe0abe474c2791546e7794cf935a8a7
bastian-js <admin@bbastian.xyz>
2023-11-15 12:01:14 +0100

    added socials.html

---
+++ index.js
@@ -0,0 +1,15 @@
+var text;
+var arrow;
+
+function changeColor() {
+  text = document.getElementById("button");
+  arrow = document.getElementById("arrow");
+
+  text.style.color = "#4287ef";
+  arrow.style.color = "#4287ef";
+}
+
+function changeColorBack() {
+  text.style.color = "#ffffff";
+  arrow.style.color = "#ffffff";
+}
