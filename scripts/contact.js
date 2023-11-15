1f1f0057fffe09fb7944452b326b0438fe21242d
Bastian <bbrugger@schueler.hakzell.at>
2023-11-15 23:29:42 +0100

    added contact.html/.js/.css

---
+++ scripts/contact.js
@@ -0,0 +1,18 @@
+function emailSend() {
+  var userName = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var senderMessage = document.getElementById("message").value;
+  var check = document.getElementById("check");
+
+  var messageBody = "Name " + userName + "<br/> Email " + email;
+  +"<br/> Message " + senderMessage;
+  Email.send({
+    SecureToken: "9843873e-4b2f-410a-8dbd-17fd5e9639e7",
+    To: "admin@bbastian.xyz",
+    From: "contact@bbastian.xyz",
+    Subject: "neue kontaktanfrage",
+    Body: messageBody,
+  }).then((message) => {
+    if (message == "OK") check.innerHTML = "email succesfully sent!";
+  });
+}
