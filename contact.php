cd271158c7fad79de6456eb7a6284cc43bd46472
bastian-js <admin@bbastian.xyz>
2023-11-15 12:59:53 +0100

    new contact form

--- contact.php
+++ contact.php
@@ -1,65 +1,47 @@
 <?php
-//send_email.php
-$email_from = "absender@domain.de";   // Absender falls keiner angegeben wurde
-$sendermail_antwort = true;      // E-Mail Adresse des Besuchers als Absender. false= Nein ; true = Ja
-$name_von_emailfeld = "Email";   // Feld in der die Absenderadresse steht
- 
-$empfaenger = "contact@bbastian.xyz"; // Empfänger-Adresse
-$mail_cc = ""; // CC-Adresse, diese E-Mail-Adresse bekommt einer weitere Kopie
-$betreff = "Neue Kontaktanfrage"; // Betreff der Email
- 
-$ok = "./mail-erfolgreich.html"; // Zielseite, wenn E-Mail erfolgreich versendet wurde
-$fehler = "./mail-fehlgeschlagen.html"; // Zielseite, wenn E-Mail nicht gesendet werden konnte
- 
- 
-// Diese Felder werden nicht in der Mail stehen
-$ignore_fields = array('submit');
-
-// Datum, wann die Mail erstellt wurde
-$name_tag = array("Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag");
-$num_tag = date("w");
-$tag = $name_tag[$num_tag];
-$jahr = date("Y");
-$n = date("d");
-$monat = date("m");
-$time = date("H:i");
- 
-// Erste Zeile unserer Email
-$msg = ">> Gesendet am $tag, $n.$monat.$jahr - $time Uhr <<\n\n";
- 
-// Hier werden alle Eingabefelder abgefragt
-foreach($_POST as $name => $value) {
-   if (in_array($name, $ignore_fields)) {
-        continue; //Ignore Felder wird nicht in die Mail eingefügt
-   }
-   $msg .= ">> $name <<\n$value\n\n";
+$errors = '';
+$myemail = 'contact@bbastian.xyz';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
 }
 
-// E-Mail Adresse des Besuchers als Absender
-if ($sendermail_antwort and isset($_POST[$name_von_emailfeld]) and filter_var($_POST[$name_von_emailfeld], FILTER_VALIDATE_EMAIL)) {
-   $email_from = $_POST[$name_von_emailfeld];
-}
- 
-$header="From: $email_from";
- 
-if (!empty($mail_cc)) {
-   $header .= "\n";
-   $header .= "Cc: $mail_cc";
-}
- 
-// Email als UTF-8 senden
-$header .= "\nContent-type: text/plain; charset=utf-8";
- 
-$mail_senden = mail($empfaenger,$betreff,$msg,$header);
- 
- 
-// Weiterleitung, hier konnte jetzt per echo auch Ausgaben stehen
-/*if($mail_senden){
-  echo('ja') // Mail wurde gesendet
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message']; 
 
-  exit();
-} else{
-  echo("nein") // Fehler beim Senden
-  exit();
-}*/
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+
+{
+
+$to = $myemail;
+
+$email_subject = "Contact form submission: $name";
+
+$email_body = "You have received a new message. ".
+
+" Here are the details:\n Name: $name \n ".
+
+"Email: $email_address\n Message \n $message";
+
+$headers = "From: $myemail\n";
+
+$headers .= "Reply-To: $email_address";
+
+mail($to,$email_subject,$email_body,$headers);
+
+//redirect to the 'thank you' page
+
+header('Location: contact-form-thank-you.html');
+
+}
 ?>
