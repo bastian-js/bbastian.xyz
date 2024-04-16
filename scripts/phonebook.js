c73e982473464e11802808bb9da95f5e3259c5f7
bastian-js <bruggi44@gmail.com>
2024-04-16 13:14:40 +0200

    implement getAll method

--- scripts/phonebook.js
+++ scripts/phonebook.js
@@ -1,4 +1,28 @@
+/*
+ Firstname
+ Lastname
+ Phone
+ Number
+ Adress
+ Place
+ PLZ
+
+ https://www.bbastian.xyz/phonebook.html?firstname=Max&lastname=Mustermann&phone=123456789&number=123&adress=Musterstraße&place=Musterstadt&plz=12345
+ */
+
 function getAll() {
   const queryString = window.location.search;
   console.log(queryString);
+
+  const firstname = urlParams.get("firstname");
+  const lastname = urlParams.get("lastname");
+  const phone = urlParams.get("phone");
+  const number = urlParams.get("number");
+  const adress = urlParams.get("adress");
+  const place = urlParams.get("place");
+  const plz = urlParams.get("plz");
+
+  const fullName = firstname + " " + lastname;
+
+  document.getElementById("name").innerHTML = fullName;
 }
