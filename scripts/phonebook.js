654f134b203b6e921a4f0d389740cb453134cc7c
bastian-js <bruggi44@gmail.com>
2024-06-09 20:53:54 +0200

    hi

--- scripts/phonebook.js
+++ scripts/phonebook.js
@@ -6,7 +6,7 @@
  Place
  PLZ
 
- https://www.bbastian.xyz/phonebook.html?firstname=Max&lastname=Mustermann&number=123&adress=Musterstraße&place=Musterstadt&plz=12345
+ https://bbastian.xyz/phonebook?firstname=Max&lastname=Mustermann&number=123&adress=Musterstraße&place=Musterstadt&plz=12345
  */
 
 function getAll() {
